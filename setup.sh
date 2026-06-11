9aa0ff8dae01628ead73877559447b0f0e5b9747
Ishan Aditya <ishan@Ishans-MacBook-Pro.local>
2026-06-11 03:41:36 +0200

    [new] setup file

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+echo ">>>>> Installing Homebrew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" </dev/null ## /dev/null skips pressing enter for the installation
+brew bundle --file=~/.dotfiles/homebrew/.Brewfile
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
